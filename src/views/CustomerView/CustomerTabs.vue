ecc7faf6bf2c297b12751a4f2851d696ba5b06c2
Ceejay Abellanosa <ceeabellanosa@proweaver.net>
2022-07-25 23:39:44 +0800

    error on customertabs fixed

--- src/views/CustomerView/CustomerTabs.vue
+++ src/views/CustomerView/CustomerTabs.vue
@@ -32,8 +32,8 @@ import {
     IonRouterOutlet,
   } from '@ionic/vue';
 import { personCircleOutline, timeOutline, homeOutline } from 'ionicons/icons';
-import { local } from '@/functions';
-import { onAdd, getChild, onDelete,ref,query, getDatabase, orderByChild, equalTo, get, onValue } from '@/firebase';
+// import { local } from '@/functions';
+// import { getChild, onDelete,ref,query, getDatabase, orderByChild, equalTo, get, onValue } from '@/firebase';
 
 export default({
   name: 'TabsPage',
@@ -55,22 +55,22 @@ export default({
     }
   },
   created(){
-    console.log(this.counter);
-    const db = getDatabase();
-    const que = query(ref(db,'/pending_tasks'),orderByChild('user_id'), equalTo(local.get('user_id')));
-    get(que).then(snapshot=>{
-        if(snapshot.exists()) for(let snap in snapshot.val()) this.counter++;
-    });
+    // console.log(this.counter);
+    // const db = getDatabase();
+    // const que = query(ref(db,'/pending_tasks'),orderByChild('user_id'), equalTo(local.get('user_id')));
+    // get(que).then(snapshot=>{
+    //     if(snapshot.exists()) for(let snap in snapshot.val()) this.counter++;
+    // });
 
-    onValue(ref(db), snapshot=>{
-      console.log(snapshot.val());
-      if(snapshot.user_id == local.get('user_id')){
-        get(que).then(snapshot=>{
-          this.counter = 0;
-            if(snapshot.exists()) for(let snap in snapshot.val()) this.counter++;
-        });
-      }
-    });
+    // onValue(ref(db), snapshot=>{
+    //   console.log(snapshot.val());
+    //   if(snapshot.user_id == local.get('user_id')){
+    //     get(que).then(snapshot=>{
+    //       this.counter = 0;
+    //         if(snapshot.exists()) for(let snap in snapshot.val()) this.counter++;
+    //     });
+    //   }
+    // });
   }
 });
 </script>
